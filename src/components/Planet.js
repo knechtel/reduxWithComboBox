760c6d32c15c17b7604c1fc2e61b5ddbfaaead67
Maiquel Knechtel <maiquelknechtel@gmail.com>
2019-11-23 22:04:24 -0300

    Evento do combobox.

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,13 +1,20 @@
-import React from 'react';
 
-class Planet extends React.Component {
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+class Planet extends Component {
     constructor() {
         super();
         this.state = {
             planets: [],
+            counter: 0
         };
     }
 
+    function(event){
+        
+        console.log("test ");
+    }
+    
     render () {
         let planets = this.props.state.planets;
         let optionItems = planets.map((planet) =>
@@ -16,12 +23,27 @@ class Planet extends React.Component {
 
         return (
          <div>
-             <select>
+             <select onChange={this.props.onValueCounter}>
                 {optionItems}
              </select>
          </div>
         )
     }
-}
 
-export default Planet;
+    
+}
+const mapStateToProps1 = state => {
+    return {
+        ctr: state.counter
+    };
+};
+
+const mapDispatchToProps1 = dispatch => {
+    
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'dec'}),
+        onValueCounter: () => dispatch({type: 'decVal',val:10})
+    };
+};
+export default connect(mapStateToProps1, mapDispatchToProps1)(Planet);
